7fda2b3aa7f41b7ae98c80bc0571153f9b35624e
yangguo <yangguo@caschina.cn>
2024-02-21 14:40:59 +0800

    配置本地化

--- shenyu-bootstrap/src/main/java/org/apache/shenyu/bootstrap/ShenyuBootstrapApplication.java
+++ shenyu-bootstrap/src/main/java/org/apache/shenyu/bootstrap/ShenyuBootstrapApplication.java
@@ -19,11 +19,13 @@ package org.apache.shenyu.bootstrap;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Shenyu bootstrap.
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ShenyuBootstrapApplication {
 
     /**
